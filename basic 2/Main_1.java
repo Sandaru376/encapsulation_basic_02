5ec9e8ec829c5712a01ff76a0e3aae42dd96ff2d
Sandaru Malith Lakshan <135944062+Sandaru376@users.noreply.github.com>
2024-02-23 21:59:20 +0530

    Add files via upload

---
+++ basic 2/Main_1.java
@@ -0,0 +1,18 @@
+public class Main_1 {
+    public static void main(String[] args) {
+        person2 obj1=new person2();
+        person2 obj2=new person2();
+
+        obj1.setname("sandaru");
+        obj1.setage(23);
+
+        
+        obj2.setname("malith");
+        obj2.setage(24);
+
+        System.out.println(obj1.getname()+obj1.getage());
+        System.out.println(obj2.getname()+obj2  .getage());
+    }
+
+    
+}
---
+++ basic 2/person2.java
@@ -0,0 +1,23 @@
+ class person2 {
+    private String name;
+    private int age;
+
+
+    public String getname(){
+        return name;
+    }
+
+    
+    public int getage(){
+        return age;
+    }
+
+    public void setname(String newNAme){
+        name=newNAme;
+    }
+    
+    
+    public void setage(int newAge){
+        age=newAge;
+    }
+}
